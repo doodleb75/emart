0cc024360ca18a86ba04521ec95f63d4e8af634a
doodleb75 <doodleb75@users.noreply.github.com>
2026-01-15 23:47:09 +0000

    deploy: 0bce6160fa38c86640edfd110491a23339ae8fa3

--- update_index.ps1
+++
@@ -1,5 +0,0 @@
-$content = [System.IO.File]::ReadAllText("c:\emart-everyday\index.html")
-$pattern = '(?m)^(\s+)<div class="unit-info">\s+<span class="strike">(.*?)</span>\s+<span class="unit-highlight">(.*?)</span>'
-$replacement = '$1<div class="unit-info">' + "`n" + '$1    <div class="price-wrap">' + "`n" + '$1        <span class="strike">$2</span>' + "`n" + '$1        <span class="unit-highlight">$3</span>' + "`n" + '$1    </div>'
-$newContent = [Regex]::Replace($content, $pattern, $replacement)
-[System.IO.File]::WriteAllText("c:\emart-everyday\index.html", $newContent, [System.Text.Encoding]::UTF8)
